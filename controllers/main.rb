d456aef9bfc5319dbacdcddc476150bc28e5ad68
Lisa Davis <oobbles@gmail.com>
2015-07-20 18:45:19 -0500

    Change all page to direct to show. Change edit page to work.

--- controllers/main.rb
+++ controllers/main.rb
@@ -4,7 +4,6 @@ end
 
 #gets all
 get "/users" do
-  binding.pry
   @users = User.all
   erb :"/users/all"
 end
@@ -19,47 +18,47 @@ post "/users/create" do
   email = params["email"]
   password = params["password"]
   the_password = BCrypt::Password.create(password)
-  @user=User.create({email: email, password: the_password})
+  @users=User.create({email: email, password: the_password})
 
 
-  if @user.valid?
+  if @users.valid?
     # Make a GET request to the following path:
-    redirect "/users/#{@user.id}"
+    redirect "/users/#{@users.id}"
   else
     erb :"users/new"
-  end 
+  end
 end
 
 
 #show one user's edit form
 get "/users/:id/edit" do
-  # @user= User.find(id)
-  erb :"users/edit"
+  @users = User.find(params[:id])
+    erb :"users/edit"
 end
 
 
 #redirecting from create
 get "/users/:id" do
-  @user = User.find(params[:id])
+  @users = User.find(params[:id])
   erb :"users/show"
 end
 
 #Where user's edit form goes TO
-put "users/:id" do
-  email = params["email"]
-  password = params["password"]
-  the_password = BCrypt::Password.create(password)
-  @user=User.update({email: email, password: the_password})
-  
-  redirect "/users/#{@user.id}"
+put "/users/:id" do
+  @users = User.find(params[:id])
+  @users.email = params[:email]
+  @users.save
+
+  redirect "/users/#{@users.id}"
 end
 
 #delete a user
 delete "/users/:id" do
-
+  User.delete(params[:id])
 end
 
 #show one user
 get "/users/:id" do
-
+  @users = User.find(params[:id])
+  erb :"users/show"
 end
